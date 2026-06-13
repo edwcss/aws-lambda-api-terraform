7ae0a1c2ce921be96228153a652435a38c6c8094
尹俊升 <wanchunsing@inshunshous-MacBook-Air.local>
2026-06-13 13:17:53 +0900

    Initial commit - Lambda API project

---
+++ main.tf
@@ -0,0 +1,60 @@
+resource "aws_iam_role" "lambda_role" {
+  name = "lambda_basic_execution"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_logs" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_lambda_function" "hello_lambda" {
+  function_name = "hello_lambda"
+  handler       = "lambda_function.handler"
+  runtime       = "python3.12"
+  role          = aws_iam_role.lambda_role.arn
+
+  filename         = "lambda.zip"
+  source_code_hash = filebase64sha256("lambda.zip")
+}
+
+resource "aws_apigatewayv2_api" "http_api" {
+  name          = "lambda-http-api"
+  protocol_type = "HTTP"
+}
+
+resource "aws_apigatewayv2_integration" "lambda_integration" {
+  api_id           = aws_apigatewayv2_api.http_api.id
+  integration_type = "AWS_PROXY"
+  integration_uri  = aws_lambda_function.hello_lambda.invoke_arn
+}
+
+resource "aws_apigatewayv2_route" "default_route" {
+  api_id    = aws_apigatewayv2_api.http_api.id
+  route_key = "GET /"
+  target    = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
+}
+
+resource "aws_lambda_permission" "api_permission" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.hello_lambda.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigatewayv2_api.http_api.execution_arn}/*/*"
+}
+
+resource "aws_apigatewayv2_stage" "default_stage" {
+  api_id      = aws_apigatewayv2_api.http_api.id
+  name        = "$default"
+  auto_deploy = true
+}
